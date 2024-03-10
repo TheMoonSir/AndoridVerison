036c721d0fdebdd85fb8ee7ec15f9255532c982d
lxzdhc <124265241+LzwCrashW@users.noreply.github.com>
2024-03-10 20:05:38 +0200

    updated and reading from docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const IndexPage = () => {
         <div className="h-[calc(100%+90px)] flex flex-col m-[0_auto] p-[0px_16px_16px] overflow-visible">
           <div className="relative grid grid-cols-[1fr_auto] w-[100%] gap-[16px] p-[0px_0px]">
             <div className="flex flex-col gap-[16px] rounded-[8px]">
-              <div className="Background relative top-[130%] flex flex-col p-[16px] w-[500px] h-[250px] rounded-[8px] shadow-none bg-[#0e0e0e] items-center place-content-center place-items-center">
+              <div className="Background relative top-[150%] flex flex-col p-[16px] w-[500px] h-[250px] rounded-[8px] shadow-none bg-[#0e0e0e] items-center place-content-center place-items-center">
                 <InputText />
               </div>
             </div>
