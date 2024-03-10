f5b03157ca8cf325e889b833bc2f712682f7912b
lxzdhc <124265241+LzwCrashW@users.noreply.github.com>
2024-03-10 19:35:35 +0200

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import "./styles/globals.css";
 import Layout from "./layout";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </Layout>
   );
 }
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 interface LayoutProps {
   children: ReactNode;
