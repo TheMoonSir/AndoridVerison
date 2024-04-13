5607efa5a1c1e47862969f8f4ea21cf5d6d524df
lxzdhc <124265241+LzwCrashW@users.noreply.github.com>
2024-04-13 15:07:24 +0300

    o

--- pages/api/v1/Andorid/[user]/SendScript.ts
+++ pages/api/v1/Andorid/[user]/SendScript.ts
@@ -38,7 +38,7 @@ export default async function handler(req: NextApiRequest) {
     if (scriptTimeout) {
       clearTimeout(scriptTimeout);
     }
-    scriptTimeout = setTimeout(clearScriptData, 35);
+    scriptTimeout = setTimeout(clearScriptData, 32);
   } else if (req.method === "GET") {
     let username = req.query?.user;
 
